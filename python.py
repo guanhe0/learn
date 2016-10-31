aa634ca2c5518f678c4c0421596f3fad2e921874
guanhe <307784732@qq.com>
2016-10-31 14:19:34 +0800

    数气球

--- python.py
+++ python.py
@@ -6,14 +6,25 @@ def judge():
     N=int(N);
     i=0;
     str_list=list();
+    count_array=[0]*N;
     while(N):
         N=N-1
         str_tmp=raw_input();
         str_list.append(str_tmp);
         i=i+1;
-    print "list len=",str_list.__len__();
+    list_len=str_list.__len__();
+
+    for i in range(len(str_list)):
+        for j in range(0,i+1):
+            if((str_list[i]==str_list[j])):
+                count_array[j]=count_array[j]+1;
+                break;
+    print count_array;
+    max=0;
     j=0;
-    while(j<i):
-        print "list[%d]=%s"%(j,str_list[j])
-        j=j+1
+    for id in range(0,list_len):
+        if(count_array[id]>max):
+            max=count_array[id];
+            j=id;
+    print "%s"%str_list[j]
 judge()
