26841da76e0aee3a58877248902a528ad24f0d0b
guanhe <307784732@qq.com>
2017-03-22 13:55:24 +0800

    add some for it

--- board_test.py
+++ board_test.py
@@ -15,3 +15,10 @@ bmc_passwd = "Huwei12#$"
 
 deactivate_sentence = "ipmitool -H %s -I lanplus -U root -P Huwei12#$ sol deactivate"
 activate_command = "ipmitool -H %s -I lanplus -U root -P Huwei12#$ sol deactivate"
+reboot_command = "ipmitool -H %s -I lanplus -U root -P Huwei12#$ power reset"
+poweroff_command = "ipmitool -H %s -I lanplus -U root -P Huwei12#$ power off"
+reset_BMC_MAC_command = "ipmitool -H %s -I lanplus -U root -P Huwei12#$ raw %s %s"
+nfs_cmds = "linux /Imgae_D05 acpi=force pcie_aspm=off rdinit=/init rdinit=/init crashkernel=256M@32M console=ttyAMA0,115200 earlycon=pl011,mmio,0x602B0000 ip=dhcp"
+
+
+
