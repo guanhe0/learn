0f1bd70381fc2890110073adbef45e0ac3cde897
guanhe <307784732@qq.com>
2017-04-12 18:49:33 +0800

    trans value frome python to shell

--- overdrive.py
+++ overdrive.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 import os
 import platform
+import time
+import string
 os.system("echo cat /proc/cmdline")
 os.system("cat /proc/cmdline")
 os.system("echo cat /proc/version")
@@ -9,25 +11,26 @@ os.system("echo uname -a")
 os.system("uname -a")
 os.system("echo df -h")
 os.system("df -h")
-os.system("echo timeout 5 ping www.baidu.com")
+os.system("echo timeout 4 ping www.baidu.com")
 os.system("timeout 5 ping www.baidu.com")
 os.system("echo fdisk -l")
-os.system("fdisk -l")
+os.system("timeout 5 fdisk -l")
 os.system("echo mkfs.ext4")
 os.system("mkfs.ext4")
-distro = 'ubuntu'
-distro = platform.dist()
-install = 'apt-get'
+distro='ubuntu'
+distro=str(platform.dist())
+install='apt-get'
 if ('Centos' in distro or 'centos' in distro):
-    install = 'yum'
-else if('Debian' in distro or 'debian' in distro):
-    install = 'yum'
-else if('Fedora' in distro or 'fedora' in distro):
-    install = 'yum'
-else if('Opensuse' in distro or 'opensuse' in distro):
-    install = 'yum'
+    install='yum'
+elif ('Debian' in distro or 'debian' in distro):
+    install='yum'
+elif ('Fedora' in distro or 'fedora' in distro):
+    install='yum'
+elif ('Opensuse' in distro or 'opensuse' in distro):
+    install='yum'
 else:
-    install = 'apt-get'
-
-os.system("echo install -y tree")
-os.system("$install install -y tree")
+    install='apt-get'
+os.environ['install']=str(install)
+os.system('echo  $install')
+#os.system("echo install -y tree")
+#os.system("$install install -y tree")
