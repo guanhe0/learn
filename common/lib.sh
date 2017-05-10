c9707fdd1ee1e70de38a87c8ad65382ae00635c8
root <root@debian.open-estuary.org>
2017-05-10 09:53:44 +0800

    add sys_info

--- common/lib.sh
+++ common/lib.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
-result=''
-if [ -e /etc/redhat-release ];then
-    result='yum'
-fi 
-echo $result
+sys_info=$(uname -a)
+distro=''
+if [ "$(echo $sys_info | grep -E 'UBUNTU|Ubuntu|ubuntu')"x != ""x ];then
+    distro="ubuntu"
+elif [ "$(echo $sys_info | grep -E 'cent|CentOS|centos')"x != ""x ];then
+    distro="centos"
+elif [ "$(echo $sys_info | grep -E 'fed|Fedora|fedora')"x != ""x ];then
+    distro="fedora"
+elif [ "$(echo $sys_info | grep -E 'DEB|Deb|deb')"x != ""x ];then
+    distro="debian"
+elif [ "$(echo $sys_info | grep -E 'OPENSUSE|OpenSuse|opensuse')"x != ""x ];then
+    distro="opensuse"
+else
+    distro="ubuntu"
+fi
+
+
+
+
